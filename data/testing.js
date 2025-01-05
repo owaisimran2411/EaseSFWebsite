d3d33b59886cd3e72ad97fee993edba621e9a097
Owais <owaisimran678@gmail.com>
2025-01-05 15:20:59 -0500

    Add Page for Displaying list of existing products

---
+++ data/testing.js
@@ -0,0 +1,61 @@
+// Sample product data
+const products = [
+    {
+        id: 1,
+        coverImage: 'https://placekitten.com/200/300',
+        title: 'Cute Kitten 1',
+        description: 'A fluffy adorable kitten ready for some snuggles.',
+        price: 19.99
+    },
+    {
+        id: 2,
+        coverImage: 'https://placekitten.com/201/300',
+        title: 'Cute Kitten 2',
+        description: 'Another cute kitten just like the other one, but different!',
+        price: 24.99
+    },
+    {
+        id: 3,
+        coverImage: 'https://placekitten.com/202/300',
+        title: 'Cute Kitten 3',
+        description: 'This kitten is very playful and ready to become your new friend.',
+        price: 29.99
+    },
+    {
+        id: 4,
+        coverImage: 'https://placekitten.com/203/300',
+        title: 'Cute Kitten 4',
+        description: 'A very sleepy kitten who likes to nap all day long',
+        price: 14.99
+    },
+    {
+         id: 5,
+         coverImage: 'https://placekitten.com/204/300',
+         title: 'Cute Kitten 5',
+         description: 'This kitten looks funny, but it is very cute!',
+         price: 12.99
+     },
+     {
+         id: 6,
+         coverImage: 'https://placekitten.com/205/300',
+         title: 'Cute Kitten 6',
+         description: 'This kitten is also very cute and fluffy.',
+         price: 22.99
+     },
+     {
+         id: 7,
+         coverImage: 'https://placekitten.com/206/300',
+         title: 'Cute Kitten 7',
+         description: 'A beautiful cat with beautiful eyes, ready for its new home.',
+         price: 18.99
+     },
+     {
+         id: 8,
+         coverImage: 'https://placekitten.com/207/300',
+         title: 'Cute Kitten 8',
+         description: 'This kitten is the cutest of the cute kittens!',
+         price: 35.99
+     },
+];
+
+export { products };
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,12 +1,28 @@
 import { Router } from "express";
 import helperMethods from "./../helpers.js";
+import { products } from "./../data/testing.js";
 
 const router = Router();
 
 // Route goes here
 router.route("/").get((req, res) => {
-	res.render("admin/index",{
+	return res.render("admin/index",{
         docTitle: "Admin - Login"
+    });
+}).post((req, res) => {
+    return res.redirect("/admin/home");
+});
+
+router.route("/home").get((req, res) => {
+    return res.render("admin/home",{
+        docTitle: "Admin - Home"
+    });
+});
+
+router.route("/products").get((req, res) => {
+    return res.render("admin/products",{
+        docTitle: "Admin - Products",
+        products: products
     });
 });
 
