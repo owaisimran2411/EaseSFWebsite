76bac91b8ddf650acd6e90dad34cbe061eaf27d7
Owais <owaisimran678@gmail.com>
2025-01-05 13:48:10 -0500

    Create Route File of adminRoutes.js using Make

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
+import adminRoute from "./adminRoutes.js";
 // import_line
 
 const constructorMethod = (app) => {	
+	app.use("/admin", adminRoute);
 	// router_line
 	app.use("*", (req, res) => {
 		res.status(404).json({
