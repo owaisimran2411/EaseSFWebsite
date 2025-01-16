4350b36f24ea162a6d6a9adac3a1d0aaa8b0f7e9
Owais <owaisimran678@gmail.com>
2025-01-15 22:06:05 -0500

    remove template file and make files

--- templateFiles/dataTemplate.js
+++
@@ -1,6 +0,0 @@
-import { COLLECTION_NAME } from "../config/mongoCollections.js";
-import helperMethods from "./../helpers.js";
-
-const methods = {};
-
-export default methods;
--- templateFiles/routerTemplate.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import helperMethods from "./../helpers.js";
-
-const router = Router();
-
-// Route goes here
-
-export default router;
