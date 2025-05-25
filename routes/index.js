e2ef357429b8f18ee62712c594540fa0b224f415
Owais <owaisimran678@gmail.com>
2025-05-25 16:06:33 -0400

    Create Route File of homeRoutes.js using Make

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 import adminRoute from "./adminRoutes.js";
 import userRoute from "./userRoutes.js";
+import homeRoute from "./homeRoutes.js";
 // import_line
 
 const constructorMethod = (app) => {	
 	app.use("/admin", adminRoute);
 	app.use("/user", userRoute);
+	app.use("/home", homeRoute);
 	// router_line
 	app.use("*", (req, res) => {
 		res.status(404).json({
